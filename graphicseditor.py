5f88940000846ac2a686637653ac9ac801e60c90
avera3142 <avera3142@gmail.com>
2023-11-11 19:01:57 +0300

    первоначальная версия проекта

---
+++ graphicseditor.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets
+from View import View
+import sys
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    g = View()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
